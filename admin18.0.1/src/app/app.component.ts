29b44811bd79262d541255ba3b2807b52af43cf6
felix <felix.renaidl@gmaio.com>
2025-04-24 13:37:17 +0700

    update 3rd party

--- admin18.0.1/src/app/app.component.ts
+++ admin18.0.1/src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./home/home.component";
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { TreeModule } from '@ali-hm/angular-tree-component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent],
+  imports: [RouterOutlet, RouterLink,TreeModule, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'admin18.0.1';
+  nodes : any  = [
+    {
+      name: 'Root 123', icon:'<i class="bi bi-folder"></i>',
+      children: [
+        { name: 'Child 1', href: 'home', icon:'' },
+        { name: 'Child 2', href: 'home2', icon:'' }
+      ]
+    }
+  ];
+
+  options : any = {};
 }
--- admin18.0.1/src/app/home/home.component.ts
+++ admin18.0.1/src/app/home/home.component.ts
@@ -3,11 +3,19 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { environment } from '../../environments/environment.development';
-
+import { FormsModule } from '@angular/forms';
+export class Actor {
+  constructor(
+    public id: number,
+    public name: string,
+    public skill: string,
+    public studio?: string,
+  ) {}
+}
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,  NgbDatepickerModule],
+  imports: [CommonModule,  FormsModule, NgbDatepickerModule],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
@@ -16,6 +24,11 @@ export class HomeComponent {
     public modalService: NgbModal, 
   ) { }
 	
+  model = new Actor(18, 'Tom Cruise', '', 'CW Productions');
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+  }
 
 	open(content: any) {
 		this.modalService.open(content,);
