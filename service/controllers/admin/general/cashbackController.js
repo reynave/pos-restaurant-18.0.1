80ca20d5ec402293b403e212c72ef06fd67c31cd
felix <felix.renaidl@gmaio.com>
2025-11-05 18:50:44 +0700

    add Cashback

--- service/controllers/admin/general/cashbackController.js
+++ service/controllers/admin/general/cashbackController.js
@@ -49,29 +49,25 @@ exports.detail = async (req, res) => {
       redeemEndDate: formatDateOnly(row.redeemEndDate),
     }));
 
-    const [cashback_amount] = await db.query(`
+     
+    const [cashback_outlet] = await db.query(`
       SELECT * 
-      FROM cashback  
-      WHERE id = ${id} AND
+      FROM cashback_outlet  
+      WHERE cashbackId = ${id} AND
       presence =1
     `);
 
-    const [cashback_outlet] = await db.query(`
-      SELECT * 
-      FROM cashback  
-      WHERE id = ${id} AND
-      presence =1
-    `);
+    
     const [cashback_payment] = await db.query(`
-      SELECT * 
-      FROM cashback  
-      WHERE id = ${id} AND
-      presence =1
+      SELECT p.id, cpt.name as 'name'
+      FROM cashback_payment  as p
+      join check_payment_type as cpt on cpt.id = p.paymentId
+      WHERE p.cashbackId = ${id} AND
+       p.presence =1
     `);
 
     const data = {
-      items: formattedRows[0],
-      amount: cashback_amount,
+      items: formattedRows[0], 
       outlet: cashback_outlet,
       payment: cashback_payment,
     }
@@ -83,6 +79,262 @@ exports.detail = async (req, res) => {
   }
 };
 
+exports.amount = async (req, res) => {
+  const id = req.query.id;
+  try {
+
+    const [cashback_amount] = await db.query(`
+      SELECT * , 0 as checkbox
+      FROM cashback_amount  
+      WHERE cashbackId = ${id} AND
+      presence =1
+    `);
+
+    const data = {
+      items: cashback_amount,
+    }
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+};
+exports.addAmount = async (req, res) => {
+  const inputDate = today();
+  try {
+
+    const [result] = await db.query(
+      `INSERT INTO cashback_amount (presence, inputDate, cashbackId ) 
+      VALUES (1, '${inputDate}', ${req.body.id})`,
+    );
+
+    res.status(201).json({
+      message: 'cashback_amount created',
+      cashback_amountId: result.insertId
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true, note: 'Database insert error' });
+  }
+};
+
+exports.selectPaymentMethod = async (req, res) => {
+  const id = req.query.id;
+  try {
+
+    const [selectPaymentMethod] = await db.query(`
+      SELECT id, name , '' as 'linkToPayment'
+      FROM check_payment_group   
+      WHERE presence =1
+      order by name asc
+    `);
+
+    for(const payment of selectPaymentMethod) {
+     const q= `SELECT c.id, c.name, t1.paymentId ,
+      CASE
+          WHEN t1.paymentId IS NULL THEN 0
+          ELSE 1
+      END AS checkbox
+      FROM check_payment_type AS c
+      LEFT JOIN (
+        SELECT p.paymentId
+            FROM cashback_payment  as p
+            join check_payment_type as cpt on cpt.id = p.paymentId
+            WHERE p.cashbackId = ${id} AND
+            p.presence =1
+      ) t1 ON t1.paymentId = c.id 
+      WHERE c.presence =1 AND c.paymentGroupId = ${payment.id}
+      order by c.name asc
+      `;
+
+     const [selectPaymentMethod] = await db.query(q);
+      payment.linkToPayment = selectPaymentMethod;
+    }
+
+    const data = {
+      items: selectPaymentMethod,
+    }
+
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database error' });
+  }
+};
+
+exports.updateDetail = async (req, res) => {
+  // const { id, name, position, email } = req.body;
+  const model = req.body.model;
+  const id = req.body.id;
+  const date = req.body.date;
+
+  const results = [];
+
+  try {
+    const [result] = await db.query(
+      `UPDATE cashback SET 
+        name = '${model['name']}',    
+        description = '${model['description']}', 
+        earningStartDate = '${date['earningStartDate']}', 
+        earningEndDate = '${date['earningEndDate']}', 
+        redeemStartDate = '${date['redeemStartDate']}', 
+        redeemEndDate = '${date['redeemEndDate']}', 
+        updateDate = '${today()}',
+        status = '${model['status']}',
+        x1 = '${model['x1']}',
+        x2 = '${model['x2']}'
+      WHERE id = ${id}`,
+    );
+
+
+    if (result.affectedRows === 0) {
+      results.push({ id, status: 'not found' });
+    } else {
+      results.push({ id, status: 'updated' });
+    }
+
+    res.json({
+      message: 'Batch update completed',
+      results: results
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database update error', details: err.message });
+  }
+};
+exports.updateAmount = async (req, res) => {
+  // const { id, name, position, email } = req.body;
+  const id = req.body.id;
+  const amounts = req.body.amounts;
+
+
+
+  if (!Array.isArray(amounts) || amounts.length === 0) {
+    return res.status(400).json({ error: 'Request body should be a non-empty array' });
+  }
+
+  const results = [];
+
+  try {
+    for (const emp of amounts) {
+      const { id } = emp;
+      if (!id) {
+        results.push({ id, status: 'failed', reason: 'Missing fields' });
+        continue;
+      }
+
+      const [result] = await db.query(
+        `UPDATE cashback_amount SET 
+          earnMin = ${emp['earnMin']},   
+          earnMax = ${emp['earnMax']},     
+          cashbackMin = ${emp['cashbackMin']},     
+          cashbackMax = ${emp['cashbackMax']},     
+          status = ${emp['status']},     
+          updateDate = '${today()}'   
+        WHERE id = ${id}`,
+      );
+
+
+      if (result.affectedRows === 0) {
+        results.push({ id, status: 'not found' });
+      } else {
+        results.push({ id, status: 'updated' });
+      }
+    }
+
+    res.json({
+      message: 'Batch update completed',
+      results: results
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database update error', details: err.message });
+  }
+};
+exports.deleteAmount = async (req, res) => {
+  // const { id, name, position, email } = req.body;
+  const id = req.body.id;
+  const amounts = req.body.amounts;
+ 
+  if (!Array.isArray(amounts) || amounts.length === 0) {
+    return res.status(400).json({ error: 'Request body should be a non-empty array' });
+  }
+
+  const results = [];
+
+  try {
+    for (const emp of amounts) {
+      const { id } = emp;
+      if (!id) {
+        results.push({ id, status: 'failed', reason: 'Missing fields' });
+        continue;
+      }
+      if (emp['checkbox'] == '1') {
+        const [result] = await db.query(
+          `UPDATE cashback_amount SET 
+            presence = 0,    
+            updateDate = '${today()}'   
+          WHERE id = ${id}`,
+        );  
+        if (result.affectedRows === 0) {
+          results.push({ id, status: 'not found' });
+        } else {
+          results.push({ id, status: 'updated' });
+        }
+      }
+    }
+
+    res.json({
+      message: 'Batch update completed',
+      results: results
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database update error', details: err.message });
+  }
+};
+
+exports.updatePaymentLink = async (req, res) => {
+  // const { id, name, position, email } = req.body;
+  const id = req.body.id;
+  const paymentMethods = req.body.paymentMethods;
+  const inputDate = today();
+  if (!Array.isArray(paymentMethods) || paymentMethods.length === 0) {
+    return res.status(400).json({ error: 'Request body should be a non-empty array' });
+  }
+
+  const results = [];
+
+  try {
+    const [result] = await db.query(
+        `DELETE FROM cashback_payment WHERE cashbackId = ${id} `
+    );
+
+
+    for (const emp of paymentMethods) {
+      const { paymentId, cashbackId } = emp; 
+     
+      const [result] = await db.query(
+        `INSERT INTO cashback_payment (paymentId, cashbackId, inputDate ) 
+        VALUES (${paymentId}, ${cashbackId}, '${inputDate}')`,
+      );
+      if (result.affectedRows === 0) {
+        results.push({ id, status: 'not found' });
+      } else {
+        results.push({ id, status: 'updated' });
+      } 
+    }
+
+    res.json({
+      message: 'Batch update completed',
+      results: results
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Database update error', details: err.message });
+  }
+};
 
 exports.postCreate = async (req, res) => {
   const model = req.body['model'];
@@ -91,21 +343,15 @@ exports.postCreate = async (req, res) => {
   try {
 
     const [result] = await db.query(
-      `INSERT INTO check_cash_type (presence, inputDate, name, value ) 
-      VALUES (?, ?, ?, ?)`,
-      [
-        1,
-        inputDate,
-        model['desc1'],
-        model['value']
-      ]
+      `INSERT INTO cashback (presence, inputDate, name, description, status ) 
+      VALUES (1, '${inputDate}', '${model['name']}', '${model['description']}', '${model['status']}')`, 
     );
 
     res.status(201).json({
       error: false,
       inputDate: inputDate,
-      message: 'check_cash_type created',
-      check_cash_typeId: result.insertId
+      message: 'cashback created',
+      id: result.insertId
     });
   } catch (err) {
     console.error(err);
@@ -136,10 +382,8 @@ exports.postUpdate = async (req, res) => {
       }
 
       const [result] = await db.query(
-        `UPDATE check_cash_type SET 
-          name = '${emp['name']}',   
-          value = '${emp['value']}',    
-          
+        `UPDATE cashback SET 
+          status = '${emp['status']}',    
           updateDate = '${today()}'
 
         WHERE id = ${id}`,
@@ -202,7 +446,7 @@ exports.postDelete = async (req, res) => {
           );
         }
       }
- 
+
     }
 
     res.json({
--- service/routes/general/cashback.js
+++ service/routes/general/cashback.js
@@ -4,8 +4,22 @@ const cb = require('../../controllers/admin/general/cashbackController');
 
 router.get('/', cb.index);
 router.get('/detail', cb.detail);
+router.post('/updateDetail', cb.updateDetail);
+
+router.get('/amount', cb.amount);
+router.post('/updateAmount', cb.updateAmount);
+router.post('/addAmount', cb.addAmount); 
+router.post('/deleteAmount', cb.deleteAmount); 
+
+router.get('/selectPaymentMethod', cb.selectPaymentMethod);
+
+//updatePaymentLink
+router.post('/updatePaymentLink', cb.updatePaymentLink);
+
 
 router.post('/update', cb.postUpdate);
+
+
 router.post('/create', cb.postCreate);
 router.post('/delete', cb.postDelete);
  
